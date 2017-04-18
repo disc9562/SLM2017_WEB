748dc98e913cfef456cbfaee75e20b90d2b3da3f
Jay <disc9562@gmail.com>
2017-04-18 11:28:48 +0800

    change dir

--- src/test/java/companyinfo/companyInfoSearchTest.java
+++ src/test/java/companyinfo/companyInfoSearchTest.java
@@ -1,7 +1,9 @@
-package tw.teddysoft.bdd.domain.invoice;
+package companyinfo;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import tw.teddysoft.bdd.domain.invoice.companyInfoSearch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 /**
@@ -10,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class companyInfoSearchTest {
     @Test
     public void company_name_should_be_teddysoftware_when_vatid_is_53909614() throws UnirestException {
-        assertThat(companyInfoSearch.getCompanyName("53909614")).isEqualTo("泰迪軟體科技有限公司");
+        Assertions.assertThat(companyInfoSearch.getCompanyName("53909614")).isEqualTo("泰迪軟體科技有限公司");
     }
 
     @Test
